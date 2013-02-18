e64629a24720b52d67842ab6588a9fcf0dc45cc5
Logan Chang <logiebear1214@gmail.com>
2013-02-18 13:55:09 -0800

    Forgot to track files lol.

---
+++ hacksassin/src/com/group1/hacksassin/CreateProfile.java
@@ -0,0 +1,23 @@
+package com.group1.hacksassin;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CreateProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.create_profile, menu);
+        return true;
+    }
+    
+}
